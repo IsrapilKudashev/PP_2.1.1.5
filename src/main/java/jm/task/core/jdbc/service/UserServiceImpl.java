511c97d850c25a95da2266fc6f2d2e9c2830f971
IsrapilKudashev <kudashev.020@gmail.com>
2023-03-11 18:21:59 +0300

    fix UserServiceImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-import org.hibernate.loader.collection.OneToManyJoinWalker;
 
 import java.util.List;
 
