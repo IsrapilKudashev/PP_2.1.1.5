2ac50e5de1f442ef229b797c2f3beca481d27ba9
IsrapilKudashev <kudashev.020@gmail.com>
2023-03-11 18:25:31 +0300

    main method implementation

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Шапи", "Юсуфов", (byte) 21);
+        userService.saveUser("Халид", "Салимов", (byte) 19);
+        userService.saveUser("Махмуд", "Валиев", (byte) 24);
+
+        userService.removeUserById(1);
+
+        List<User> usersList = userService.getAllUsers();
+        usersList.forEach(user -> System.out.println(user.toString()));
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
