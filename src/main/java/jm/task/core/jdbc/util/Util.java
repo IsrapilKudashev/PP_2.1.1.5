30d44e3d1151203904f4c91cc5cc4e27e99344b2
IsrapilKudashev <kudashev.020@gmail.com>
2023-03-11 17:46:39 +0300

    hibernate setup and getSessionFactory method

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/user";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        Properties properties = new Properties();
+        try {
+            if (sessionFactory == null) {
+                properties.put(Environment.URL, URL);
+                properties.put(Environment.USER, USERNAME);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, DIALECT);
+                properties.put(Environment.DRIVER, DRIVER);
+                properties.put(Environment.SHOW_SQL, true);
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "");
+
+                sessionFactory = new Configuration().addProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
 }
